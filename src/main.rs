1e4916522690fa2d0c07147ee244e18ad72c4424
Val <valuedmammal@protonmail.com>
2023-05-21 15:24:49 -0400

    use serde_json Map

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 #![warn(clippy::pedantic)]
-use bitcoin::{Address, Denomination, Network};
-use bitcoin::secp256k1::PublicKey;
+use bitcoin::Denomination;
 use bitcoin_waila::PaymentParams;
 use clap::{command, Parser};
 use core::str::FromStr;
-use lightning_invoice::Invoice;
-use lnurl::lightning_address::LightningAddress;
-use lnurl::lnurl::LnUrl;
 use nostr::nips::nip19::ToBech32;
-use secp256k1::XOnlyPublicKey;
-use serde::Serialize;
+use serde_json::{Map, Value, json};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    #[arg(short = 'a', long, help = "Show all results including None type", requires = "query")]
+    all: bool,
+    
+    #[arg(short = 'n', long, help = "Expose NIPs (experimental)", requires = "query")]
+    nips: bool,
+
     #[arg(short = 'p', long, help = "Pretty printed JSON", requires = "query")]
     pretty: bool,
 
@@ -28,43 +29,6 @@ struct Args {
 
     #[arg(help = "bitcoin string to parse", required(true))]
     query: String,
-}
-
-#[derive(Debug, Serialize)]
-enum NostrValue {
-    Hex(XOnlyPublicKey),
-    Bech32(String),
-}
-
-#[derive(Debug, Serialize)]
-struct Base {
-    kind: String,
-    network: Option<Network>,
-    address: Option<Address>,
-    invoice: Option<Invoice>,
-    pubkey: Option<PublicKey>,
-    amount: Option<String>,
-    memo: Option<String>,
-    lnurl: Option<LnUrl>,
-    lnaddr: Option<LightningAddress>,
-    nostr: Option<Vec<NostrValue>>,
-}
-
-impl Base {
-    fn new() -> Self {
-        Base {
-            kind: String::new(),
-            network: None,
-            address: None,
-            invoice: None,
-            pubkey: None,
-            amount: None,
-            memo: None,
-            lnurl: None,
-            lnaddr: None,
-            nostr: None,
-        }
-    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -83,6 +47,19 @@ impl From<serde_json::Error> for Error {
     }
 }
 
+static KEYS: &[&str] = &[
+    "kind",
+    "network",
+    "address",
+    "invoice",
+    "pubkey",
+    "amount",
+    "memo",
+    "lnurl",
+    "lnaddr",
+    "nostr",
+];
+
 type Result<T> = core::result::Result<T, Error>;
 
 fn main() -> Result<()> {
@@ -95,27 +72,11 @@ fn main() -> Result<()> {
         _ => Denomination::Satoshi,
     };
 
-    let map = parse_params(&s, unit)?;
-
-    let json_out = if args.pretty {
-        serde_json::to_string_pretty(&map)?
-    } else {
-        serde_json::to_string(&map)?
-    };
-
-    println!("{json_out}");
-
-    Ok(())
-}
-
-fn parse_params(s: &str, unit: Denomination) -> Result<Base> {
-    let Ok(parsed) = PaymentParams::from_str(s) else {
+    let Ok(parsed) = PaymentParams::from_str(&s) else {
         return Err(
             Error::ParseParamsError("not a known bitcoin string")
         )
     };
-
-    let mut m = Base::new();
 
     // Any additional PaymentParams variants must be included here
     let kind = match parsed {
@@ -128,37 +89,154 @@ fn parse_params(s: &str, unit: Denomination) -> Result<Base> {
         PaymentParams::LightningAddress(_) => "LnAddress",
         PaymentParams::Nostr(_) => "NostrValue",
     };
-
-    // Currently supported methods on a PaymentParams with optional
-    // return type. Additional methods should be added here
-    m.kind = String::from(kind);
-    m.network = parsed.network();
-    m.address = parsed.address();
-    m.invoice = parsed.invoice();
-    m.pubkey = parsed.node_pubkey();
-    if let Some(amount) = parsed.amount() {
-        m.amount = Some(amount.to_string_with_denomination(unit));
-    }
-    m.memo = parsed.memo();
-    m.lnurl = parsed.lnurl();
-    m.lnaddr = parsed.lightning_address();
-    if let Some(k) = parsed.nostr_pubkey() {
-        let bech32 = k.to_bech32().unwrap(); // handle bech32::encode err?
-        let v = vec![NostrValue::Hex(k), NostrValue::Bech32(bech32)];
-        m.nostr = Some(v);
-    }
-
-    Ok(m)
-}
-
-#[test]
-fn not_a_bitcoin_string() {
-    let bad_string = "notabitcoinstring";
-    let unit = Denomination::Satoshi;
-
-    assert_eq!(
-        parse_params(bad_string, unit).unwrap_err(),
-        Error::ParseParamsError("not a known bitcoin string")
-    );
-}
+    let kind = String::from(kind);
     
+    let mut map = Map::new();
+    map.insert(KEYS[0].into(), Value::String(kind));    
+    
+    if args.all {
+        map = build(&parsed, map, unit);
+    } else {
+        map = build_sparse(&parsed, map, unit);
+    };
+
+    if args.nips {
+        map.insert(KEYS[9].into(), parse_nostr(&parsed));
+    }
+
+    let json_out = if args.pretty {
+        serde_json::to_string_pretty(&map)?
+    } else {
+        serde_json::to_string(&map)?
+    };
+
+    println!("{json_out}");
+    
+    Ok(())
+}
+
+fn build(parsed: &PaymentParams, mut map: Map<String, Value>, unit: Denomination) -> Map<String, Value> {
+    
+    // net, addr, inv, pubk, amt, memo, lnurl, lnaddr
+    let val = if let Some(n) = parsed.network() { 
+        Value::String(n.to_string()) 
+    } else { 
+        json!(null) 
+    };
+    map.insert(KEYS[1].into(), val);
+    
+    let val = if let Some(a) = parsed.address() { 
+        Value::String(a.to_string()) 
+    } else { 
+        json!(null) 
+    };
+    map.insert(KEYS[2].into(), val);
+    
+    let val = if let Some(i) = parsed.invoice() { 
+        Value::String(i.to_string()) 
+    } else { 
+        json!(null) 
+    };
+    map.insert(KEYS[3].into(), val);
+    
+    let val = if let Some(k) = parsed.node_pubkey() { 
+        Value::String(k.to_string()) 
+    } else { 
+        json!(null) 
+    };
+    map.insert(KEYS[4].into(), val);
+    
+    let val = if let Some(a) = parsed.amount() { 
+        Value::String(a.to_string_with_denomination(unit)) 
+    } else { 
+        json!(null) 
+    };
+    map.insert(KEYS[5].into(), val);
+    
+    let val = if let Some(m) = parsed.memo() { 
+        Value::String(m)
+    } else { 
+        json!(null) 
+    };
+    map.insert(KEYS[6].into(), val);
+    
+    let val = if let Some(u) = parsed.lnurl() { 
+        Value::String(u.to_string()) 
+    } else { 
+        json!(null) 
+    };
+    map.insert(KEYS[7].into(), val);
+    
+    let val = if let Some(a) = parsed.lightning_address() { 
+        Value::String(a.to_string()) 
+    } else { 
+        json!(null) 
+    };
+    map.insert(KEYS[8].into(), val);
+    
+    map
+}
+
+fn build_sparse(parsed: &PaymentParams, mut map: Map<String, Value>, unit: Denomination) -> Map<String, Value> {
+    
+    // net, addr, inv, pubk, amt, memo, lnurl, lnaddr
+    if let Some(n) = parsed.network() {
+        let v = Value::String(n.to_string());
+        map.insert(KEYS[1].into(), v);
+    }
+    
+    if let Some(a) = parsed.address() {
+        let v = Value::String(a.to_string());
+        map.insert(KEYS[2].into(), v);
+    }
+    
+    if let Some(i) = parsed.invoice() {
+        let v = Value::String(i.to_string());
+        map.insert(KEYS[3].into(), v);
+    }
+    
+    if let Some(k) = parsed.node_pubkey() {
+        let v = Value::String(k.to_string());
+        map.insert(KEYS[4].into(), v);
+    }
+    
+    if let Some(amt) = parsed.amount() {
+        let s = amt.to_string_with_denomination(unit);
+        let v = Value::String(s);
+        map.insert(KEYS[5].into(), v);
+    }
+    
+    if let Some(m) = parsed.memo() {
+        map.insert(KEYS[6].into(), Value::String(m));
+    }
+    
+    if let Some(u) = parsed.lnurl() {
+        let v = Value::String(u.to_string());
+        map.insert(KEYS[7].into(), v);
+    }
+    
+    if let Some(a) = parsed.lightning_address() {
+        let v = Value::String(a.to_string());
+        map.insert(KEYS[8].into(), v);
+    }
+
+    map
+}    
+    
+fn parse_nostr(parsed: &PaymentParams) -> serde_json::Value {
+    if let Some(k) = parsed.nostr_pubkey() {
+        let bech32_str = k.to_bech32().unwrap(); // todo: catch bech32::encode err
+        let mut bech32 = String::from("bech32: ");
+        bech32.push_str(&bech32_str);
+        
+        let hex_str = k.to_string();
+        let mut hex = String::from("hex: ");
+        hex.push_str(&hex_str);
+        
+        Value::Array(
+            vec![Value::String(hex), Value::String(bech32)]
+        )
+    } else {
+        json!(null)
+    }
+}
